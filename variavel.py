b605ca5685b71549bb0c18abea0f5a502c35766b
vininini189 <viniciuslopes.galileu@gmail.com>
2025-05-29 17:33:39 -0300

    Add files via upload

---
+++ variavel.py
@@ -0,0 +1,5 @@
+#vou usar input() para armazenar uma informação em uma variavel
+nome=input("qual nome vc quer armazenar?")
+
+#vou utilizar o print imprimir a informação da variavel nome
+print(f'o nome armazenado é de: {nome}')
