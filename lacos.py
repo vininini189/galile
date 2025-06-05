5389adbadeb6a948e9cc9651be8224d6179e80a3
vininini189 <viniciuslopes.galileu@gmail.com>
2025-06-05 17:10:08 -0300

    Add files via upload

---
+++ lacos.py
@@ -0,0 +1,20 @@
+#criar uma variavel chamada soma 
+#logo em seguida,quero que vcs somem o numero 1 no valor antigo dessa variavel
+#printem o resultado separadamente,ou seja 1,2,3,4,5,6,7,8,9,10
+
+soma = 0
+
+for i in range(10): #para cada i dentro do intervalo até 10,quero que rode o seguinte codigo
+    soma = soma + 1 
+    print(soma)
+
+#while True: #colocando uma condição
+#    print('4rhugrbg4ubg4yhyrghrbgrhtbgrthgb onishi é japones,chines e koreanotrjrthunthrtnhtrnbrtbnrtijbntrijbntrbjintrbijtrnbijnbjgrnrjbnrthrhbnntjbnrbjnbjnb')
+
+soma = 0
+while soma < 1000: #enquanto a variavel soma for menor do que 1000
+    soma = soma + 10 
+    print(soma) #logo em seguida, quero que imprima o resultado em tela 
+
+
+
