785f3f4e241ac6d5776d827e450ab89a7ff69172
vininini189 <viniciuslopes.galileu@gmail.com>
2025-06-05 17:33:29 -0300

    Add files via upload

---
+++ funções.py
@@ -0,0 +1,12 @@
+def formula_maluca (numero):
+    return print(numero + (1 * 10 -20 / 2 + 500))
+
+#vou chamar a função
+formula_maluca(4)
+def texto_noob (nome):
+    print(f'o {nome} é noob no fortnite')
+
+
+
+#quero chamar a função texto_noob
+texto_noob("vitor")
