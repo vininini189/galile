bcf514fe5f345931a4491a9f519e6c5e9d02d236
vininini189 <viniciuslopes.galileu@gmail.com>
2025-06-10 17:32:57 -0300

    Add files via upload

---
+++ string.py
@@ -0,0 +1,21 @@
+nome_aluno = input("digite o nome do aluno: ")
+
+#imprimindo o indice numero 2, ou seja, a 3° letra de algum 
+print(f'a 3° letra da posição do nome  é {nome_aluno[2]}')
+
+#acessando a ultima letra de qualquer nome 
+print(f'a ultima  letra do nome é {nome_aluno [-1]}')
+
+#criando duas variaveis de numero num1 e num2
+num1 = int(input("digite o nome do num1: "))
+num2 = int(input("digite o nome do num2: "))
+
+print(f'a soma dos dois numeros é de {num1 + num2}')
+#usei f strings para fazer a soma direta de num1 com num2 
+
+
+#Usando f stirings para definir a quantidade de casas decimais 
+brunão = 434.73267420845654656757675756756757567
+print(f'o numero com 2 casas decimais é: {brunão:.2f}')
+
+
