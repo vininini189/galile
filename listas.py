b5636a2f121c7285812bce78f98f50771030e22f
vininini189 <viniciuslopes.galileu@gmail.com>
2025-06-24 17:35:11 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,18 @@
+#listas
+
+#analise de medio de notas
+lista_media_notas = [1.5, 0.1, 1.6, 2.0]
+media = sum (lista_media_notas) / len(lista_media_notas)
+print(media)
+
+lista_nomes = ['Marchalfa ', 'abujabu', 'vininini', 'nishi','alvare']
+
+
+#imprima o nomes de todos!
+#imprimir o nome 
+
+print(lista_nomes[0])
+print(lista_nomes[1])
+print(lista_nomes[2])
+print(lista_nomes[3])
+print(lista_nomes[4])
