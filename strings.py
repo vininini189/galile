9f0b276a52e2b04dcea0326b29324f41f8c1b8ea
vininini189 <viniciuslopes.galileu@gmail.com>
2025-06-24 16:46:16 -0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,8 @@
+frase = "                                              VIniCiUs é NOob no RUBlOX                                                   "
+
+#Metodo .strip(), exlcui  os espaços iniciais e os finais
+print(frase.strip())
+#técnica de encadeamento de metodos
+frase3 = '     bem-vindo ao PYTHON!'
+frase3_formatada =frase3.strip().capitalize().replace('python','Onishi' )
+print(frase3_formatada)
