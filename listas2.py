0e0746e50b9468189ba0592df1a0d606b8e8e5a2
vininini189 <viniciuslopes.galileu@gmail.com>
2025-06-26 17:24:20 -0300

    Add files via upload

---
+++ listas2.py
@@ -0,0 +1,8 @@
+lista_clientes = [
+    ['álvaro', 'alvaro.sampaio@galileunegocios.com.br,', 32],
+    ['felipe', 'felipe.nasciben@galileunegocios.com.br', 41],
+    ['matheus','matheus.biasotto@galileunegocios.com.br',29],
+]
+print(lista_clientes[1][2])
+print(lista_clientes[2][1])
+print(lista_clientes[0][1])
