2d41ae40db8a713b51d31810f062ac51bcb79f5c
vininini189 <viniciuslopes.galileu@gmail.com>
2025-07-10 17:20:04 -0300

    Add files via upload

--- desafiolistas.py
+++ desafiolistas.py
@@ -4,8 +4,17 @@ arenas =  [
     [2000, 1900, 1950, 2100, 2200], # arena 3
 ]
 
-for arena in arenas: 
-    
-    arenas_medias = sum(arena[1]) / len(arena)
+contador = 0 
+lista_medias = []
 
-    print(f'a arenas do arena {arenas_medias} é de {arenas}')
+
+
+#criei um laço para precorrer a lista arenas 
+for a in arenas: #'para cada a (arena) dentro da lista arenas
+      contador += 1 #estou somando 1 do  antigo valor do variavel contador 
+      media_arenas = sum(a) / len(a)
+      lista_medias.append(media_arenas) #adicinando a media calculada na lista_medias
+      print(f'a media da arena {contador} é de: {media_arenas}')
+      print(lista_medias)
+
+print(f'a maior pontuação entre as arenas é de: {max(lista_medias)}')
