c30da203a421a271c66d034c7374153e4457eedc
vininini189 <viniciuslopes.galileu@gmail.com>
2025-07-17 17:34:14 -0300

    Add files via upload

---
+++ tupla.py
@@ -0,0 +1,7 @@
+# tupla 
+roupas_lista = ["camisa", "calça", "blazer"]
+roupas_lista[0] = "camiseta" # ok 
+print(roupas_lista)
+# tupla 
+roupas_tupla= ("camisa", "calça", "blazer")
+roupas_tupla[0] = "camiseta" # erro! tupla não podem ser alterados
