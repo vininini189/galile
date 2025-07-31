e3330aea54c615a0c7d99d46899378494029f3dc
vininini189 <viniciuslopes.galileu@gmail.com>
2025-07-31 16:56:49 -0300

    Add files via upload

--- tupladesafio.py
+++ tupladesafio.py
@@ -5,9 +5,19 @@ estoque = (
     ("webcam hd", 0)
 )
 
+contador_zeros = 0 
+soma = 0
 print('produtos com menos de 5 unidades')
-for i in estoque :
-    if i[1] < 5:
-        print(f'- {i[0]}')
+for i in estoque : #usei uma estrutura de laço para percorrer todos os itens da loja 
+    if i[1] < 5: #verificaram se cada item tinha ou não 5 unidades
+        print(f'- {i[0]}') #se a linha de cima é vdd, imprimir a quantidade 
+    if i[1] == 0:
+        contador_zeros += 1
+    soma += i[1] #estou somando cada item da tupla estoque 
+print(f'produtos esgotasos {contador_zeros}')
+print(f'a soma dos produtos é: {soma}')
 
-    
+
+
+
+#programen a quantidade de itens zerados dentro da tupla estoque 
