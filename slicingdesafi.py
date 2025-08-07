ada24ca9f3c8a2a1de20ecbfe8c00b92af3b8aaa
vininini189 <viniciuslopes.galileu@gmail.com>
2025-08-07 16:40:48 -0300

    Add files via upload

---
+++ slicingdesafi.py
@@ -0,0 +1,10 @@
+nomes = ('alvaro',
+         'leticia',
+         'bruno',
+        'henrique',
+        'carlos')
+print(nomes[1:4]) #('leticia', 'bruno', 'henrique')
+print(nomes[0:3]) #('alvaro', 'leticia', 'bruno')
+print(nomes[2:5]) #('bruno', 'henrique', 'carlos')
+print(nomes[0:  :2]) #('alvaro', 'bruno', 'carlos')
+print(nomes[4:  :-1]) #('carlos', 'henrique', 'bruno', 'leticia', 'alvaro')
