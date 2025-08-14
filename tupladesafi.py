33406219c9e61c1e3b3f8aff034d34f24764f2bf
vininini189 <viniciuslopes.galileu@gmail.com>
2025-08-14 17:03:42 -0300

    Add files via upload

--- tupladesafi.py
+++ tupladesafi.py
@@ -7,13 +7,33 @@ pedidos = [
     ("Notebook", "Eletrônicos", 3299.90, "Entregue"),
     ("Jaqueta", "Vestuário", 139.90, "Cancelado"),
 ]
-soma_entregoes = []
-soma = sum (soma_entregues)
-for i in Entregue : 
-    soma_entregoes.append(i[2])
-    
+soma_produtos_entregues = []
+#somar valor de todos os produtos entregues
+for i in pedidos : 
+    if i[3] == 'Entregue' : 
+        soma_produtos_entregues.append(i[2])
+
+    print(f'a soma todos os produtos entregues é : {sum(soma_produtos_entregues)}')
+  
 print('protutos entregues'.center(40,'-'))
 for i in pedidos: 
     if i[3] == 'Entregue':
         print(f' -  {i[0]}')
+
+#percorrer a lista de pedidos
+#averificar se a categoria está como vestuária e se o  status 
+
+soma_cancelado_vestuario = 0 
+
+for i in pedidos: #percorerrendo a lista de pedidos 
+    if i[3] == 'Cancelado' and i[1] == 'Vestuário' : 
+            soma_cancelado_vestuario += 1 #soma 1 do valor antigo da variável soma_cancelado_vesturio
+print(f'Cancelado_cancelado da categoria vestuário:{soma_cancelado_vestuario}')
+
+
+filtro = input('Digite uma categoria para filtra:  ')
+
+for i in pedidos: 
+     if filtro == i[1]:
+          print(f'- {i[0]}')
 
