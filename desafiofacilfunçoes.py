245789b6473a7881c6a20ffde95c11e54d717d90
vininini189 <viniciuslopes.galileu@gmail.com>
2025-10-02 16:12:16 -0300

    Add files via upload

--- desafiofacilfunçoes.py
+++ desafiofacilfunçoes.py
@@ -3,22 +3,26 @@ lista_notas_champagnat = [2.5 , 2.9 , 0.2 , 2.9]
 
 def calcular_media (lista_notas):
     media = sum(lista_notas) / len(lista_notas)
-    return print(f'a media das notas é de : {media}')
+    return media
 
 calcular_media(lista_notas_champagnat)
 
-nome_do_aluno = input("digite sua media")
-nota_do_aluno = float(input("parabens pela sua nota"))
-
+nome_do_aluno = input("digite sua o nome do aluno: ")
+nota_do_aluno = calcular_media(lista_notas_champagnat)
 
 def avaliar_aluno (nota_aluno, nome_aluno):
     if nota_aluno >= 9 and nota_aluno <= 10:
         print(f'parabéns, {nome_aluno}. excelente media!!!!!!!!!!!!!!!')
-        avaliar_aluno(9, 'Nishi')
 
-nome_do_aluno = input("digita sua nota ")
-nota_do_aluno = float(input("tá media legal"))
-        
-if nota_aluno >= 7 and nota_aluno >= 8:
-            print(f'parabens {nome_do_aluno} pelo menos vc esta na media né')
+    if nota_aluno >= 7 and nota_aluno <= 8.9:
+        print(f'Foi, {nome_aluno}. Passou!!!!!!!!!!!!!!!!')
+
+    if nota_aluno >= 5 and nota_aluno <= 6.9:
+        print(f'Tome cuidado, {nome_aluno}. Quase não passou!!!!!!!!!!!!!!!!')
+
+    if nota_aluno >= 0 and nota_aluno <= 5:
+        print(f'Passou não! {nome_aluno}')
+
+avaliar_aluno(nota_do_aluno, nome_do_aluno)
+
             
