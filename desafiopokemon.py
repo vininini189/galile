72e45b60e0f7ad2a8b03dd388e37dabda018302c
vininini189 <viniciuslopes.galileu@gmail.com>
2025-10-16 17:22:39 -0300

    Add files via upload

--- desafiopokemon.py
+++ desafiopokemon.py
@@ -1,6 +1,16 @@
 import random
 
-def selecionar_pokemon():
+pokemon = {
+    'bundaassada': {'ataque': 'bola de fogo','dano':35 },
+    'jatodeagua': {'ataque': 'jato de agua', 'dano': 32},
+    'carnemoida': {'ataque': 'chicote de carne','dano':30}
+}
+
+
+jogador1 = ''
+jogador2 = ''
+
+def selecionar_pokemon(jogador):
     print('selecione seu pokemon'.center(40, '-'))
     print(" (1) bundaassada \n (2) jatodeagua \n (3) carnemoida \n ")
     print('_'*40)
@@ -11,14 +21,25 @@ def selecionar_pokemon():
         escolha = 'jatodeagua'
     elif escolha == 3: 
        escolha = 'carnemoida'
+    else:
+        print(f'não entendo isso seu bostacocopintinhoamarelotodomybrotherMYBESTOFREND❤️❤️❤️❤️❤️❤️❤️❤️😍😍😍😍😍😍😍❤️😍❤️💕❤️❤️😍❤️❤️❤️💕😘💕❤️😍❤️❤️💕😘💕💕😍❤️❤️❤️❤️😁😘😘💕❤️😍❤️❤️❤️💕😘😘💕❤️😍❤️❤️💕😘💕💕🤣❤️❤️😍😍❤️❤️❤️❤️❤️💕😘😘😍❤️')
+
+    return escolha 
+
+escolha1 = selecionar_pokemon(jogador1)
+escolha2 = selecionar_pokemon(jogador2)
+
+def ficha_habilidades(escolha):
+    print(f'ficha de habilidade'.center(40, '-'))
+    print(f'ataque: {pokemon[escolha]['ataque']} e Dano: {pokemon[escolha]['dano']}')
     
-    elif print( digite um numero de 1,2 ou 3 ): 
+ficha_habilidades(escolha1)
+print('-'*40)
+ficha_habilidades(escolha2)
 
-
-selecionar_pokemon():
-
-pokemon = {
-    'bundaassada': {'ataque': 'bola de fogo','dano':35 },
-    'jatodegua': {'ataque': 'jato de agua', 'dano': 32},
-    'carmemoida': {'ataque': 'chicote de carne','dano':30}
-}
+def dano_critico (escolha):
+    dano_critico = pokemon[escolha]['dano'] * random.uniform(1,2)
+    print(f'O dano_critico do {escolha} é de {dano_critico:.2f}')
+    
+dano_critico(escolha1)
+dano_critico(escolha2)
